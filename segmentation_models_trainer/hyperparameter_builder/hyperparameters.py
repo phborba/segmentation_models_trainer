0485e017408222ba3a45bbeb43856ad4a9dc133c
phborba <philipeborba@gmail.com>
2020-09-14 18:27:29 -0300

    hyperparameter with test

---
+++ segmentation_models_trainer/hyperparameter_builder/hyperparameters.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ segmentation_models_trainer
+                              -------------------
+        begin                : 2020-09-14
+        git sha              : $Format:%H$
+        copyright            : (C) 2020 by Philipe Borba - Cartographic Engineer @ Brazilian Army
+        email                : philipeborba at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ****
+"""
+
+from segmentation_models_trainer.hyperparameter_builder.optimizer import Optimizer
+
+from dataclasses import dataclass
+from dataclasses_jsonschema import JsonSchemaMixin
+
+@dataclass
+class Hyperparameters(JsonSchemaMixin):
+    batch_size: int
+    optimizer: Optimizer
+
+if __name__ == '__main__':
+    x = Optimizer(name='Adam',config={'learning_rate':0.01})
+    y = Hyperparameters(batch_size=16, optimizer=x)
+    print(y.to_json())
---
+++ tests/test_hyperparameters.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ segmentation_models_trainer
+                              -------------------
+        begin                : 2020-09-14
+        git sha              : $Format:%H$
+        copyright            : (C) 2020 by Philipe Borba - Cartographic Engineer 
+                                                            @ Brazilian Army
+        email                : philipeborba at gmail dot com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ****
+"""
+
+import unittest
+import json
+from segmentation_models_trainer.hyperparameter_builder.hyperparameters import \
+    Hyperparameters
+from segmentation_models_trainer.hyperparameter_builder.optimizer import \
+    Optimizer
+
+class Test_TestHyperparameters(unittest.TestCase):
+    opt = Optimizer(name='Adam',config={'learning_rate':0.01})
+    hp = Hyperparameters(batch_size=16, optimizer=opt)
+    json_text = '''{"name": "Adam", "config": {"learning_rate": 0.01}}'''
+
+    def test_create_instance(self):
+        self.assertEqual(
+            self.hp.batch_size, 16
+        )
+        self.assertEqual(
+            self.hp.optimizer, self.opt
+        )
+        self.assertEqual(
+            self.opt.to_json(), self.json_text
+        )
